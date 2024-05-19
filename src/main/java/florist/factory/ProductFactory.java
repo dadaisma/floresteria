6761e5e6f01ac673946e5d16be9c713d9814d1c0
dadaisma <infodadaisma@gmail.com>
2024-05-19 10:39:10 +0200

    productandpersitencejson

--- src/main/java/florist/factory/ProductFactory.java
+++ src/main/java/florist/factory/ProductFactory.java
@@ -1,6 +1,29 @@
 package florist.factory;
 
+
+import florist.models.Product;
+
+import java.util.Map;
+
 public class ProductFactory {
 
-
+    public Product createProduct(String type, Object... params) {
+        Map<String, Object> parameters = Map.of(params);
+        switch (type) {
+            case "Tree":
+                double height = (double) parameters.get("height");
+                double treePrice = (double) parameters.get("price");
+                return new Tree(height, treePrice);
+            case "Flower":
+                String color = (String) parameters.get("color");
+                double flowerPrice = (double) parameters.get("price");
+                return new Flower(color, flowerPrice);
+            case "Decoration":
+                Material material = (Material) parameters.get("material");
+                double decorationPrice = (double) parameters.get("price");
+                return new Decoration(material, decorationPrice);
+            default:
+                throw new IllegalArgumentException("Unknown product type: " + type);
+        }
+    }
 }
