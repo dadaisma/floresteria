bfbf9f32c137c4007c4a5a6895228d01708c6eb8
Diana <dianafrancitorraferran@gmail.com>
2024-05-20 12:13:14 +0200

    comentario de prueva

--- src/main/java/florist/models/Florist.java
+++ src/main/java/florist/models/Florist.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Florist {
+    //prova
     private String name;
     private List<Product> stock;
     private List<Ticket> tickets;
